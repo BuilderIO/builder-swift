2e83160952fca8ea7d4b829363d790f6659a9a55
shyam-builder <shyam@builder.io>
2022-12-07 19:51:51 +0530

    Support for image contain / cover, and horizontal alignment

--- Sources/BuilderIO/Blocks/BuilderImage.swift
+++ Sources/BuilderIO/Blocks/BuilderImage.swift
@@ -1,22 +1,37 @@
 import SwiftUI
+private typealias CSS = CSSStyleUtil
 
 @available(iOS 15.0, macOS 10.15, *)
 struct BuilderImage: View {
     var image: String
-    var backgroundSize: String
+    var backgroundSize: String;
+    var aspectRatio: CGFloat;
+    var responsiveStyles: [String: String]?;
+
     
     var body: some View {
+        let currentScale = UIScreen.main.scale;
+        let foregroundColor = CSS.getColor(value: responsiveStyles?["color"] ?? "black");
+        let bgColor = CSS.getColor(value: responsiveStyles?["backgroundColor"] ?? "white");
+        let cornerRadius = CSS.getFloatValue(cssString:responsiveStyles?["borderRadius"] ?? "0px")
+        let horizontalAlignmentFrame = CSS.getFrameFromHorizontalAlignment(styles: responsiveStyles ?? [:]);
+        let maxWidth = CSS.getFloatValue(cssString: responsiveStyles?["maxWidth"], defaultValue: .infinity) ;
+
         BackportAsyncImage(url: URL(string: image)) { phase in
             if let image = phase.image {
                 image
                     .resizable()
-                    .aspectRatio(contentMode: backgroundSize == "cover" ? .fill : .fit)
-            }else if phase.error != nil {
+                    .aspectRatio(1/aspectRatio, contentMode: backgroundSize == "cover" ? .fill : .fit)
+                    .frame(width: maxWidth)
+            } else if phase.error != nil {
                 Color.red
             } else {
                 Color.blue
             }
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .padding(CSS.getBoxStyle(boxStyleProperty: "padding", finalStyles: responsiveStyles ?? [:])) // padding for the button
+        .frame(idealWidth: horizontalAlignmentFrame.idealWidth, maxWidth: horizontalAlignmentFrame.maxWidth, alignment: horizontalAlignmentFrame.alignment)
+        .foregroundColor(foregroundColor)
+        .background(RoundedRectangle(cornerRadius: cornerRadius).fill(bgColor))
     }
 }
