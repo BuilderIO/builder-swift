254dcbc5bfdb78a6d4b359c7eeac804d53422fe1
Steve Sewell <steve@builder.io>
2023-06-06 14:59:33 -0700

    add preview param

--- Sources/BuilderIO/Helpers/Content.swift
+++ Sources/BuilderIO/Helpers/Content.swift
@@ -1,12 +1,16 @@
 import Foundation
 
 public struct Content {
-    public static func getContent(model: String, apiKey: String, url: String, locale: String?, callback: @escaping ((BuilderContent?)->())) {
+    public static func getContent(model: String, apiKey: String, url: String, locale: String?, preview: Boolean?, callback: @escaping ((BuilderContent?)->())) {
         let encodedUrl = String(describing: url.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!)
         var str = "https://cdn.builder.io/api/v2/content/\(model)?apiKey=\(apiKey)&url=\(encodedUrl)"
         
         if let locale = locale, !locale.isEmpty {
             str += "&locale=\(locale)"
+        }
+        
+        if let preview = preview {
+            str += "&preview=true"
         }
         
         let url = URL(string: str)!
