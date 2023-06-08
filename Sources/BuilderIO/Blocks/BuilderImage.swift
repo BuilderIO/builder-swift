c85a95d78c6e408b35c366f9c08a588da3d999ca
Shyam Seshadri <97863898+shyam-builder@users.noreply.github.com>
2023-06-08 20:04:44 +0530

    Fix issues with vertical stack spacing (#11)
    * fix for spacing in vstack

--- Sources/BuilderIO/Blocks/BuilderImage.swift
+++ Sources/BuilderIO/Blocks/BuilderImage.swift
@@ -10,19 +10,24 @@ struct BuilderImage: View {
 
     
     var body: some View {
-        let currentScale = UIScreen.main.scale;
+        let _ = UIScreen.main.scale;
         let foregroundColor = CSS.getColor(value: responsiveStyles?["color"] ?? "black");
         let bgColor = CSS.getColor(value: responsiveStyles?["backgroundColor"] ?? "white");
         let cornerRadius = CSS.getFloatValue(cssString:responsiveStyles?["borderRadius"] ?? "0px")
         let horizontalAlignmentFrame = CSS.getFrameFromHorizontalAlignment(styles: responsiveStyles ?? [:]);
         let maxWidth = CSS.getFloatValue(cssString: responsiveStyles?["maxWidth"], defaultValue: .infinity) ;
+//        let _ = print("BACKGROUND SIZE ----", backgroundSize, " CONTENT MODE ", backgroundSize == "cover" ? ContentMode.fill : ContentMode.fit);
+//        let _ = print("ASPECT RATIO", aspectRatio, 1/aspectRatio)
+//        let _ = print("Max Width?", maxWidth == .infinity ? nil : maxWidth);
 
         BackportAsyncImage(url: URL(string: image)) { phase in
             if let image = phase.image {
                 image
                     .resizable()
                     .aspectRatio(1/aspectRatio, contentMode: backgroundSize == "cover" ? .fill : .fit)
-                    .frame(width: maxWidth)
+                    .frame(width: maxWidth == .infinity ? nil : maxWidth)
+//                    .border(.blue)
+
             } else if phase.error != nil {
                 Color.red
             } else {
