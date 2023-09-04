7e6fa125e23d933b808c07236d1ef4a46b891ac8
shyam-builder <shyam@builder.io>
2023-09-04 17:47:45 +0530

    more logs

--- Sources/BuilderIO/Helpers/Content.swift
+++ Sources/BuilderIO/Helpers/Content.swift
@@ -1,9 +1,6 @@
 import Foundation
 import os
 
-@available(iOS 14.0, *)
-let logger = Logger(subsystem: Bundle.main.bundleIdentifier!, category: "builder")
-
 public struct Content {
     public static func getContent(model: String, apiKey: String, url: String, locale: String? = nil, preview: String? = nil, callback: @escaping ((BuilderContent?)->())) {
         let encodedUrl = String(describing: url.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!)
@@ -11,12 +8,14 @@ public struct Content {
         
         let overrideLocale = UserDefaults.standard.string(forKey: "builderLocale")
         let overridePreviewContent = UserDefaults.standard.string(forKey: "builderContentId")
+        print("override preview content", overridePreviewContent ?? "no override");
         
         let useLocale = overrideLocale ?? locale
         let usePreview = overridePreviewContent ?? preview
         
         if let localPreview = usePreview, !localPreview.isEmpty {
             str += "/\(localPreview)"
+            print("LOCAL PREVIEW YES!");
         }
         str += "?apiKey=\(apiKey)&url=\(encodedUrl)"
         
@@ -33,11 +32,7 @@ public struct Content {
         let url = URL(string: str)!
         
         let session = !(usePreview ?? "").isEmpty ? URLSession(configuration: .ephemeral) : URLSession.shared
-        if #available(iOS 14.0, *) {
-            logger.info("Fetching URL = \(url)")
-        } else {
-            // Fallback on earlier versions
-        }
+        print("Fetching URL = \(url)")
         let task = session.dataTask(with: url) {(data, response, error) in
             guard let data = data else {
                 callback(nil)
@@ -47,11 +42,8 @@ public struct Content {
             let jsonString = String(data: data, encoding: .utf8)!
             do {
                 if let localPreview = usePreview, !localPreview.isEmpty {
-                    if #available(iOS 14.0, *) {
-                        logger.info("Fetched FROM LOCAL PREVIEW = \(url)")
-                    } else {
-                        // Fallback on earlier versions
-                    }
+                    print("Fetched FROM LOCAL PREVIEW = \(url)")
+                    
                     let content = try decoder.decode(BuilderContent.self, from: Data(jsonString.utf8))
                     callback(content)
                 } else {
