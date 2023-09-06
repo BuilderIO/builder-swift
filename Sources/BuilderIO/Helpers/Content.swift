2973d0c86e54b7514ec3e25b8fd7d1d85edd1a27
Shyam Seshadri <97863898+shyam-builder@users.noreply.github.com>
2023-09-06 18:58:06 +0530

    [ENG-4207] Refactor and clean up for the SDK  (#15)
    * Add logging
    
    * add shake notification
    
    * Extract more helpers out

--- Sources/BuilderIO/Helpers/Content.swift
+++ Sources/BuilderIO/Helpers/Content.swift
@@ -1,18 +1,28 @@
 import Foundation
+import os
 
 public struct Content {
+    
+    public static func isPreviewing() -> Bool {
+        // Assuming we are only using appetize for previewing in
+        // the content editor for now
+        let isAppetize = UserDefaults.standard.bool(forKey: "isAppetize");
+        return isAppetize;
+    }
     public static func getContent(model: String, apiKey: String, url: String, locale: String? = nil, preview: String? = nil, callback: @escaping ((BuilderContent?)->())) {
         let encodedUrl = String(describing: url.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!)
         var str = "https://cdn.builder.io/api/v3/content/\(model)"
         
-        let overrideLocale = UserDefaults.standard.string(forKey: "builderLocale")
-        let overridePreviewContent = UserDefaults.standard.string(forKey: "builderContentId")
+        let overrideLocale = UserDefaults.standard.string(forKey: "builderLocale");
+        let overridePreviewContent = UserDefaults.standard.string(forKey: "builderContentId");
+        print("override preview content", overridePreviewContent ?? "no override");
         
         let useLocale = overrideLocale ?? locale
         let usePreview = overridePreviewContent ?? preview
         
         if let localPreview = usePreview, !localPreview.isEmpty {
             str += "/\(localPreview)"
+            print("LOCAL PREVIEW YES!");
         }
         str += "?apiKey=\(apiKey)&url=\(encodedUrl)"
         
@@ -29,7 +39,7 @@ public struct Content {
         let url = URL(string: str)!
         
         let session = !(usePreview ?? "").isEmpty ? URLSession(configuration: .ephemeral) : URLSession.shared
-        
+        print("Fetching URL = \(url)")
         let task = session.dataTask(with: url) {(data, response, error) in
             guard let data = data else {
                 callback(nil)
@@ -39,6 +49,8 @@ public struct Content {
             let jsonString = String(data: data, encoding: .utf8)!
             do {
                 if let localPreview = usePreview, !localPreview.isEmpty {
+                    print("Fetched FROM LOCAL PREVIEW = \(url)")
+                    
                     let content = try decoder.decode(BuilderContent.self, from: Data(jsonString.utf8))
                     callback(content)
                 } else {
