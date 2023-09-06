8cd5ee5d450a47fb07cef6585befc4a47e90b2f4
shyam-builder <shyam@builder.io>
2023-09-06 16:47:30 +0530

    add shake notification

--- Sources/BuilderIO/Helpers/ShakeNotification.swift
+++ Sources/BuilderIO/Helpers/ShakeNotification.swift
@@ -1,20 +1,12 @@
-//
-//  File.swift
-//  
-//
-//  Created by Shyam Seshadri on 9/6/23.
-//
 
 import Foundation
 import UIKit
 
-extension NSNotification.Name {
-    public static let deviceDidShakeNotification = NSNotification.Name("BuilderIOShakeNotification")
-}
+public let deviceDidShakeNotification = NSNotification.Name("BuilderIOShakeNotification")
 
 extension UIWindow {
     open override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
         super.motionEnded(motion, with: event)
-        NotificationCenter.default.post(name: .deviceDidShakeNotification, object: event)
+        NotificationCenter.default.post(name: deviceDidShakeNotification, object: event)
     }
 }
