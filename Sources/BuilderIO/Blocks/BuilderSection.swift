79c4e3947561571aa2ba6bcfd0b31d967de99a63
shyam-builder <shyam@builder.io>
2023-09-28 18:56:30 +0530

    check

--- Sources/BuilderIO/Blocks/BuilderSection.swift
+++ Sources/BuilderIO/Blocks/BuilderSection.swift
@@ -3,26 +3,19 @@ import SwiftUI
 
 private typealias CSS = CSSStyleUtil
 
-struct BuilderColumn: Codable {
-    var blocks: [BuilderBlock] = []
-}
 
 @available(iOS 15.0, macOS 10.15, *)
-struct BuilderColumns: View {
-    var columns: [BuilderColumn]
-    var space: CGFloat = 0
+struct BuilderSection: View {
     var responsiveStyles: [String: String]?;
+    var children: [BuilderBlock]?;
     
     @available(iOS 15.0, *)
     var body: some View {
         let bgColor = CSS.getColor(value: responsiveStyles?["backgroundColor"]);
         let cornerRadius = CSS.getFloatValue(cssString:responsiveStyles?["borderRadius"] ?? "0px")
-        let _ = print("COLUMN FOUND WITH STYLES_____", responsiveStyles ?? "NO RESPONSIVE STYLES");
+        let _ = print("SECTION FOUND WITH STYLES_____", responsiveStyles ?? "NO RESPONSIVE STYLES");
         VStack {
-            ForEach(0...columns.count - 1, id: \.self) { index in
-                let blocks = columns[index].blocks
-                RenderBlocks(blocks: blocks)
-            }
+            RenderBlocks(blocks: children!)
         }.background(RoundedRectangle(cornerRadius: cornerRadius).fill(bgColor))
             
         
