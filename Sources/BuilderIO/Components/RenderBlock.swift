ba6b4624a7d4d1aa0a37dfac531baf5b51126fe1
shyam-builder <shyam@builder.io>
2023-09-28 21:19:28 +0530

    check

--- Sources/BuilderIO/Components/RenderBlock.swift
+++ Sources/BuilderIO/Components/RenderBlock.swift
@@ -24,7 +24,7 @@ struct RenderBlock: View {
                 }
                 
                 if name == nil || (componentDict[name!] != nil && block.component?.options! != nil) {
-                    let _ = print("No Name for component or no factory", name!)
+                    let _ = print("No Name for component or no factory", name ?? "NO NAME")
                     if block.children != nil {
                         RenderBlocks(blocks: block.children!)
                     }
