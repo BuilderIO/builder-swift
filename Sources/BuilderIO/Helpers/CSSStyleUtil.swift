5dda4bffef25bae3b31740171013f379c5c616e7
shyam-builder <shyam@builder.io>
2023-09-29 16:26:12 +0530

    check

--- Sources/BuilderIO/Helpers/CSSStyleUtil.swift
+++ Sources/BuilderIO/Helpers/CSSStyleUtil.swift
@@ -83,8 +83,7 @@ class CSSStyleUtil {
         return Color(
             red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
             green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
-            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
-            alpha: CGFloat(1.0)
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0
         )
     }
     
