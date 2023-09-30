4db4834784b974906789dca9257fe409c33bae18
shyam-builder <shyam@builder.io>
2023-09-30 19:09:31 +0530

    check

--- Sources/BuilderIO/Components/RenderBlock.swift
+++ Sources/BuilderIO/Components/RenderBlock.swift
@@ -46,7 +46,7 @@ struct RenderBlock: View {
                 .frame(minWidth: 0, idealWidth: .infinity, maxWidth: .infinity, alignment: alignment)
                 .cornerRadius(cornerRadius)
         } else {
-            view.background(Color.clear)
+            view.background(Color.white)
                 .padding(CSS.getBoxStyle(boxStyleProperty: "margin", finalStyles: finalStyles))
                 .multilineTextAlignment(textAlignValue == "center" ? .center : textAlignValue == "right" ? .trailing : .leading)
                 .frame(minWidth: 0, idealWidth: .infinity, maxWidth: .infinity, alignment: alignment)
