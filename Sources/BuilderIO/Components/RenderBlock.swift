bff618e1a3d298c52dbd11e109feb5fd4e490ddc
shyam-builder <shyam@builder.io>
2023-10-01 09:56:22 +0530

    try with rectangles

--- Sources/BuilderIO/Components/RenderBlock.swift
+++ Sources/BuilderIO/Components/RenderBlock.swift
@@ -17,44 +17,58 @@ struct RenderBlock: View {
         let cornerRadius = CSS.getFloatValue(cssString:finalStyles["borderRadius"] ?? "0px")
         let borderWidth = CSS.getFloatValue(cssString:finalStyles["borderWidth"] ?? "0px")
         let borderColor = CSS.getColor(value: finalStyles["borderColor"] ?? "none");
-        let padding = CSS.getBoxStyle(boxStyleProperty: "padding", finalStyles: finalStyles)
         let alignment = horizontalAlignment == HorizontalAlignment.LeftAlign ? Alignment.leading : (horizontalAlignment == HorizontalAlignment.Center ? Alignment.center : Alignment.trailing)
-        
+        let name = block.component?.name
+        let isEmptyView = (name == nil || componentDict[name!]  == nil) && block.children == nil;
         if  finalStyles["display"] != "none" {
-            VStack(alignment: .center, spacing: 0) {
-                
-                let name = block.component?.name
-                if name != nil {
-                    let factoryValue = componentDict[name!]
+            if (isEmptyView) {
+                Rectangle()
+                    .if(hasMinHeight) { view in
+                        view.frame(minWidth: 0, idealWidth: .infinity, maxWidth: .infinity, minHeight: 10, idealHeight: 10, alignment: alignment)
+                    }
+                    .if(!hasMinHeight) { view in
+                        view.frame(minWidth: 0, idealWidth: .infinity, maxWidth: .infinity, alignment: alignment)
+                    }
+                    .if(hasBgColor) { view in
+                        view.foregroundColor(bgColor)
+                    }
                     
-                    if factoryValue != nil && block.component?.options! != nil {
-                        AnyView(_fromValue: factoryValue!(block.component!.options!, finalStyles, block.children))
+            } else {
+                VStack(alignment: .center, spacing: 0) {
+                    
+                    if name != nil {
+                        let factoryValue = componentDict[name!]
+                        
+                        if factoryValue != nil && block.component?.options! != nil {
+                            AnyView(_fromValue: factoryValue!(block.component!.options!, finalStyles, block.children))
+                        }
                     }
+                    
+                    if name == nil || !(componentDict[name!] != nil && block.component?.options! != nil) {
+    //                    let _ = print("No Name for component or no factory", name ?? "NO NAME")
+                        if block.children != nil {
+                            RenderBlocks(blocks: block.children!)
+                        }
+                    }
+                }
+
+                .padding(CSS.getBoxStyle(boxStyleProperty: "padding", finalStyles: finalStyles))
+    //            .if(hasBgColor) { view in
+    //                view.background(bgColor)
+    //            }
+                .background(Color.purple)
+                .padding(CSS.getBoxStyle(boxStyleProperty: "margin", finalStyles: finalStyles))
+                .multilineTextAlignment(textAlignValue == "center" ? .center : textAlignValue == "right" ? .trailing : .leading)
+                .if(hasMinHeight) { view in
+                    view.frame(minWidth: 0, idealWidth: .infinity, maxWidth: .infinity, minHeight: 10, idealHeight: 10, alignment: alignment)
+                }
+                .if(!hasMinHeight) { view in
+                    view.frame(minWidth: 0, idealWidth: .infinity, maxWidth: .infinity, alignment: alignment)
                 }
                 
-                if name == nil || !(componentDict[name!] != nil && block.component?.options! != nil) {
-//                    let _ = print("No Name for component or no factory", name ?? "NO NAME")
-                    if block.children != nil {
-                        RenderBlocks(blocks: block.children!)
-                    }
-                }
-            }
-
-            .padding(CSS.getBoxStyle(boxStyleProperty: "padding", finalStyles: finalStyles))
-//            .if(hasBgColor) { view in
-//                view.background(bgColor)
-//            }
-            .background(Color.purple)
-            .padding(CSS.getBoxStyle(boxStyleProperty: "margin", finalStyles: finalStyles))
-            .multilineTextAlignment(textAlignValue == "center" ? .center : textAlignValue == "right" ? .trailing : .leading)
-            .if(hasMinHeight) { view in
-                view.frame(minWidth: 0, idealWidth: .infinity, maxWidth: .infinity, minHeight: 10, idealHeight: 10, alignment: alignment)
-            }
-            .if(!hasMinHeight) { view in
-                view.frame(minWidth: 0, idealWidth: .infinity, maxWidth: .infinity, alignment: alignment)
+                .cornerRadius(cornerRadius)
             }
             
-            .cornerRadius(cornerRadius)
           
         }
 
