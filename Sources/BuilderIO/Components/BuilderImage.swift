9c6a510287ab0a520e8b4ed8ee56aa3b8a21b901
aaron-demelo <aarondemelo@gmail.com>
2025-06-19 23:31:56 +0530

    added image support. resolved fomats

--- Sources/BuilderIO/Components/BuilderImage.swift
+++ Sources/BuilderIO/Components/BuilderImage.swift
@@ -1,50 +1,46 @@
 import SwiftUI
-import SwiftyJSON
 
 struct BuilderImage: BuilderViewProtocol {
-  var componentType: BuilderComponentType = .image
-
-  var block: BuilderBlockModel
-  var responsiveStyles: [String: String]?
-  var aspectRatio: CGSize?
-  var children: [BuilderBlockModel]?
-
-  var imageURL: URL?
-
-  init(block: BuilderBlockModel) {
-    self.block = block
-    self.responsiveStyles = getFinalStyle(responsiveStyles: block.responsiveStyles)
-    self.imageURL = URL(string: block.component?.options?["image"].string ?? "")
-    self.children = block.children
-
-    if let ratio = block.component?.options?["aspectRatio"].doubleValue {
-      self.aspectRatio = CGSize(width: ratio, height: 1)
-    } else {
-      self.aspectRatio = nil
+    var componentType: BuilderComponentType = .image
+    
+    var block: BuilderBlockModel
+    var children: [BuilderBlockModel]?
+    
+    var imageURL: URL?
+    
+    @State private var imageLoadedSuccessfully: Bool = false
+    
+    
+    init(block: BuilderBlockModel) {
+        self.block = block
+        self.imageURL = URL(string: block.component?.options?["image"].string ?? "")
+        self.children = block.children
     }
-
-  }
-
-  var body: some View {
-    AsyncImage(url: imageURL) { phase in
-      switch phase {
-      case .empty:
-        ProgressView()
-      case .success(let image):
-        image.resizable()
-          .if(aspectRatio != nil) { view in
-            view.aspectRatio(self.aspectRatio!, contentMode: .fit)
-          }.border(Color.yellow)
-      case .failure:
-        Color.gray
-      @unknown default:
-        EmptyView()
-      }
-    }.overlay(content: {
-      if let children = children {
-        BuilderBlock(blocks: children)
-      }
-    })
-
-  }
+    
+    var body: some View {
+        // Create a ZStack to layer the image and its children
+        ZStack {
+            AsyncImage(url: imageURL) { phase in
+                switch phase {
+                case .empty:
+                    ProgressView()
+                case .success(let image):
+                    image.resizable()
+                        .clipped().zIndex(0)
+                        .if((children?.count ?? 0) > 0) { view in
+                            view.overlay(content: {
+                                if let children = children {
+                                    BuilderBlock(blocks: children).zIndex(1)
+                                }
+                            })
+                        }
+                case .failure:
+                    Color.gray
+                @unknown default:
+                    EmptyView()
+                }
+            }
+        }
+        
+    }
 }
